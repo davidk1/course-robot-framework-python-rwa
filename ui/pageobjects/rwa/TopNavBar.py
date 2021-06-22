979a283feb291fb0e106ff87d2d60be65e8f6c29
rfautotrain <rfautotrain@gmail.com>
2021-06-22 15:45:04 +0000

    test-push

--- ui/pageobjects/rwa/TopNavBar.py
+++ ui/pageobjects/rwa/TopNavBar.py
@@ -5,3 +5,10 @@ from ui.pageobjects.decorators import capture_screenshot_on_failure
 class TopNavBar(BasePage):
     """Sdilene komponenty pro vsechny stranky aplikace"""
 
+    new_transaction_button = "CSS:[data-test=nav-top-new-transaction]"
+
+    @capture_screenshot_on_failure
+    def click_new_transaction_button(self):
+        """Stiskne tlacitko '$ NEW' pro otevreni nove transakce"""
+        self.logger.console('  ..klikne na tlacitko "$ NEW"')
+        self._wait_and_click_element(self.new_transaction_button)
