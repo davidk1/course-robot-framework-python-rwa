d67b6c31259be675760eb66148c40ead5484b5e0
rfautotrain <rfautotrain@gmail.com>
2021-06-23 06:16:48 +0000

    srovnani s masterem

--- ui/pageobjects/rwa/NewTransactionPage.py
+++ ui/pageobjects/rwa/NewTransactionPage.py
@@ -12,16 +12,16 @@ class NewTransactionPage(BasePage):
 
     @capture_screenshot_on_failure
     def select_recipient_by_name(self, name='Edgar Johns'):
-        """Vybere prijemce platby kliknutim na jeho jmeno
+        """Vybere prijemce platby kliknutim na jeho jmeno.
 
-        :param name: prijemce plagtby
+        :param name: prijemce platby
         """
         selector = self.recipient_name_label.format(name=name)
         self._wait_and_click_element(selector)
 
     @capture_screenshot_on_failure
     def enter_transaction_amount(self, amount=12):
-        """Vyplni vysi transakce
+        """Vyplni vysi transakce.
 
         :param amount: vyse transakce [int / str]
         """
@@ -29,13 +29,14 @@ class NewTransactionPage(BasePage):
 
     @capture_screenshot_on_failure
     def enter_transaction_description(self, description):
-        """Vyplni vysi transakce
+        """Vyplni pole pro popis platby.
 
         :param description: popis platby [str]
         """
         self._wait_for_element_and_type_text(self.transaction_description_field, description)
 
+    @capture_screenshot_on_failure
     def submit_transaction(self):
-        """ Stiskne tlacitko 'PAY' pro odeslani platby."""
+        """Stiskne tlacitko `PAY` pro odeslani platby."""
         self._wait_and_click_element(self.submit_transaction_button)
         self.selib.wait_until_page_contains("Paid ", 15)
